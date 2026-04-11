1d637a35ee030f62abd3ba39df5e4e6e159ff066
Joshua Clay <josh.t.clay1995@gmail.com>
2026-04-11 00:59:30 -0700

    set up router shell and placeholder pages

---
+++ src/pages/PhrasesQuiz.jsx
@@ -0,0 +1,10 @@
+function PhrasesQuiz() {
+  return (
+    <section>
+      <h2>Japanese Phrases Quiz</h2>
+      <p>Quiz questions and score tracking will go here.</p>
+    </section>
+  );
+}
+
+export default PhrasesQuiz;
