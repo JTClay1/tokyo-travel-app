366d699c992344f679ef06bb5742efdeae27bd5a
Joshua Clay <josh.t.clay1995@gmail.com>
2026-04-18 16:39:49 -0700

    add ticking tokyo time clock to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+function getTokyoTime() {
+  return new Intl.DateTimeFormat("en-GB", {
+    timeZone: "Asia/Tokyo",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  }).format(new Date());
+}
+
 function Navbar({ theme, onToggleTheme }) {
+  const [tokyoTime, setTokyoTime] = useState(getTokyoTime());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTokyoTime(getTokyoTime());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <nav className="navbar">
       <h1 className="nav-logo">Tokyo Travel App</h1>
@@ -11,6 +32,11 @@ function Navbar({ theme, onToggleTheme }) {
           <NavLink to="/weather">Weather</NavLink>
           <NavLink to="/restaurants">Restaurants</NavLink>
           <NavLink to="/phrases-quiz">Phrases Quiz</NavLink>
+        </div>
+
+        <div className="tokyo-clock" aria-label="Current Tokyo time">
+          <span className="tokyo-clock-label">Tokyo Time</span>
+          <span className="tokyo-clock-time">{tokyoTime}</span>
         </div>
 
         <button
